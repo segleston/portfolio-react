261fae737bc6292887cdad2b795959a311d4b0e9
segleston <suraheg@gmail.com>
2024-02-17 15:19:32 +0000

    deployed

--- src/components/Pages/ProjectGallery.jsx
+++ src/components/Pages/ProjectGallery.jsx
@@ -3,15 +3,15 @@ import projects from '../../projects.json'
 
 const ProjectGallery = () => {
 
-  return (
-    <>
-      <h1>These are my projects!</h1>
+   return (
+      <>
+         <h1>These are my projects!</h1>
 
-      {projects.map(project => (
-        <Project key={project.id} project={project} />
-      ))}
-    </>
-  )
+         {projects.map(project => (
+            <Project key={project.id} project={project} />
+         ))}
+      </>
+   )
 }
 
 export default ProjectGallery;
