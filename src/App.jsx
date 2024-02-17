bae1747593997c4495b7acffb1aae430eb07e739
segleston <suraheg@gmail.com>
2024-02-17 14:18:29 +0000

    added gh-pages, updated vite.config and Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MyNav from './components/Navbar'
+import { Contact, Home, ProjectGallery } from './components/Pages';
 import './App.css'
 
 
 function App() {
   return (
-    <>
+    <Router basename={'./portfolio-react'}>
       <MyNav />
-    </>
+      <Routes>
+        <Route path='/portfolio' element={<ProjectGallery />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='' element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
--- src/components/Pages/ProjectGallery.jsx
+++ src/components/Pages/ProjectGallery.jsx
@@ -2,15 +2,15 @@ import Project from '../Project';
 import projects from '../../projects.json'
 
 const ProjectGallery = () => {
-   return(
-   <>
-     <h1>Project Gallery</h1>
-     (projects.map(project => (
-        <Project key ={project.id} project={project}/>
+   return (
+      <>
+         <h1>Project Gallery</h1>
+         (projects.map(project = (
+         <Project key={project.id} project={project} />
 
-     )))
-    </>
-    )
+         )))
+      </>
+   )
 }
 
 export default ProjectGallery;
