4463afedb8615f9082ec5c60bfef90565d6ab2b0
segleston <suraheg@gmail.com>
2024-02-17 13:45:06 +0000

    added map over projects

--- src/components/Pages/ProjectGallery.jsx
+++ src/components/Pages/ProjectGallery.jsx
@@ -1,9 +1,14 @@
 import Project from '../Project';
+import projects from '../../projects.json'
 
 const ProjectGallery = () => {
    return(
    <>
-     
+     <h1>Project Gallery</h1>
+     (projects.map(project => (
+        <Project key ={project.id} project={project}/>
+
+     )))
     </>
     )
 }
