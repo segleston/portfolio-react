1c7da02e15ddd35faa0a96526004e40aaae054d5
segleston <suraheg@gmail.com>
2024-02-19 12:14:42 +0000

    added css to cards

--- src/components/Pages/ProjectGallery.jsx
+++ src/components/Pages/ProjectGallery.jsx
@@ -2,22 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import projects from '../../projects.json';
 import ProjectCard from '../Project';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
 
 function ProjectGallery() {
    const projectList = projects.map(project => (
-      <Link key={project.id} to={`/project/${project.id}`}>
-         <ProjectCard project={project} />
-      </Link>
+      <div key={project.id} className="col-md-4 mb-5">
+         <Link to={`/project/${project.id}`}>
+            <ProjectCard project={project} />
+         </Link>
+      </div>
    ));
 
    return (
-      <div>
-         <h1>Porfolio Gallery</h1>
-         <div className='row'>
+      <div className='container'>
+         <h1 className='heading-font p-5 m-5 text-center'>Portfolio Gallery</h1>
+         <div className="row">
             {projectList}
          </div>
       </div>
    );
 }
+
 
 export default ProjectGallery;
--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -6,18 +6,29 @@ import Button from 'react-bootstrap/Button';
 
 export default function ProjectCard({ project }) {
     return (
-            <a href={`/project/${project.id}`}>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                        <Card.Title>{project.title}</Card.Title>
-                        <Card.Text>
-                            {project.description}
-                        </Card.Text>
-                        <a href={project.deployed}><Button variant="primary">Deployed site</Button></a>
-                        <a href={project.github}><Button variant="primary">Github Repo</Button></a>
-                    </Card.Body>
-                </Card>
-            </a>  
-                )
+        <a href={`/project/${project.id}`}>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>{project.title}</Card.Title>
+                    <Card.Text>
+                    {project.description}
+                    </Card.Text>
+                    <a href={project.deployed}><Button variant="primary" className='m-1 card-button'>Deployed site</Button></a>
+                    <a href={project.github}><Button variant="primary" className='card-button'>Github Repo</Button></a>
+                </Card.Body>
+            </Card>
+            {/* <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                    <Card.Title>{project.title}</Card.Title>
+                    <Card.Text>
+                        {project.description}
+                    </Card.Text>
+                    <a href={project.deployed}><Button variant="primary">Deployed site</Button></a>
+                    <a href={project.github}><Button variant="primary">Github Repo</Button></a>
+                </Card.Body>
+            </Card> */}
+        </a>
+    )
 }
