5961b6ef30f3b183e93c526b130221d1a6c511df
segleston <suraheg@gmail.com>
2024-02-19 13:26:25 +0000

    added container and row/columns to contact me page

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -8,26 +8,33 @@ const Contact = () => {
                 <div className='d-flex justify-content-around'>
                 <h1 className='heading-font p-5 m-5 text-center'>Contact me</h1>
                 </div>
-                <div>
+                <div  className="container">
+                    <div className='row'>
+                        <div className='col-md-6'>
                 <Form>
                     <Form.Group>
                         <Form.Label>Enter your full name:</Form.Label>
                         <Form.Control type="text"
-                            placeholder="Enter your full name" />
+                            placeholder="Full Name" />
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Enter your email address:</Form.Label>
                         <Form.Control type="email"
-                            placeholder="Enter your your email address" />
+                            placeholder="Email" />
                     </Form.Group>
                     <Form.Group className="mb-3" >
                         <Form.Label>Your message:</Form.Label>
-                        <Form.Control as="textarea" aria-label="With textarea" placeholder="Enter your your message"  />
+                        <Form.Control as="textarea" aria-label="With textarea" placeholder="Message"  />
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Submit
                     </Button>
                 </Form>
+                </div>
+                <div className='col-md-6'>
+                <h1>TEST</h1>
+                </div>
+                </div>
                 </div>
             </div>
         </>
