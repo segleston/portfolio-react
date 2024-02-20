ecb6f6b3e55a5345f2995e956855269b7fbe4f5b
segleston <suraheg@gmail.com>
2024-02-20 16:20:11 +0000

    uncommented toggle on nav

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -8,15 +8,12 @@ function MyNav() {
     <Navbar expand="lg" className="my-nav fixed-top">
       <Container className=''>
         <Navbar.Brand href="#home" className="mr-auto px-5">SE</Navbar.Brand>
-        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto px-5">
-            {/* <Nav.Link href="./" className='px-3'>Home</Nav.Link> */}
             <Link to="/" className='px-3 nav-link'>Home</Link>
             <Link to="/portfolio" className='px-3 nav-link'>Portfolio</Link>
             <Link to="/contact" className='px-3 nav-link'>Contact</Link>
-            {/* <Nav.Link href="./portfolio" className='px-3'>Portfolio</Nav.Link>
-            <Nav.Link href="./contact" className='px-3'>Contact</Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
