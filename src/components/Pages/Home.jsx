9a3bd85799223fa2758a8025e586757e0f46c251
segleston <segleston@gmail.com>
2024-05-30 21:53:08 +0100

    updated text on home page

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -31,52 +31,62 @@ function Home() {
                 </div>
 
                 {/* Cards Section */}
-                <Container className='container-background'>
+                <Container className='container-background border-left border-right'>
                     <div className='featured-projects-header'>
                         Featured Projects
                     </div>
-                    <Link to={`/project/2`}>
-                        <Row className="my-5 card-background">
-                            <Col lg={8} sm={12} className="d-flex align-items-center justify-content-center">
-                                <div className='card-content'>
-                                    <h2 className='card-title'>devOpps</h2>
-                                    <p className='card-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nisi, totam, magni tenetur corrupti similique maiores expedita reiciendis aut enim illum dignissimos iure ipsam maxime quasi accusamus dolorem quam modi?</p>
-                                </div>
-                            </Col>
-                            <Col lg={4} sm={12} className='photo-card'>
-                                <img src={CardImage1} className='img-fluid card-image' alt='Card Image 1' />
-                            </Col>
-                        </Row>
-                    </Link>
-                    <Link to={`/project/1`}>
-                        <Row className="my-5 card-background">
-                            <Col lg={4} sm={12} className='photo-card'>
-                                <img src={CardImage2} className='img-fluid card-image' alt='Card Image 2' />
-                            </Col>
-                            <Col lg={8} sm={12} className="d-flex align-items-center justify-content-center">
-                                <div className='card-content'>
-                                    <h2 className='card-title'>Movie Blast from the Past</h2>
-                                    <p className='card-text'>Card content goes here. This card has the image on the left and text on the right.</p>
-                                </div>
-                            </Col>
-                        </Row>
-                    </Link>
-                    <Link to={`/project/3`}>
-                        <Row className="my-5 card-background">
-                            <Col lg={8} sm={12} className="d-flex align-items-center justify-content-center">
-                                <div className='card-content'>
-                                    <h2 className='card-title'>Weather Dashboard</h2>
-                                    <p className='card-text'>Card content goes here. This card has the image on the right and text on the left.</p>
-                                </div>
-                            </Col>
-                            <Col lg={4} sm={12} className='photo-card'>
-                                <img src={CardImage3} className='img-fluid card-image' alt='Card Image 3' />
-                            </Col>
-                        </Row>
-                    </Link>
+
+                    <Row className="my-5 card-background">
+                        <Col lg={8} sm={12} className="d-flex align-items-center justify-content-center">
+                            <div className='card-content text-start'>
+                                <h2 className='card-title p-3 text-start'>devOpps</h2>
+                                <p className='card-text p-3 text-start'>devOpps is a dynamic web platform connecting developers with job opportunities and employers, featuring job listings, personalized profiles, and intuitive search functionalities. Developed using React, HTML5, CSS3, JavaScript, Node.js, and Tailwind CSS, it offers a seamless experience on all devices.</p>
+                                <Link to="https://devopps.app/" className='button-text'> <button>Application</button> </Link>
+                                <Link to="https://github.com/segleston/DevOpps" className='button-text'> <button>Repository</button> </Link>
+                            </div>
+                        </Col>
+
+                        <Col lg={4} sm={12} className='photo-card'>
+                            <img src={CardImage1} className='img-fluid card-image' alt='Card Image 3' />
+                        </Col>
+                    </Row>
+
+                    {/* </Link> */}
+                    {/* <Link to={`/project/1`}> */}
+                    <Row className="my-5 card-background">
+                        <Col lg={4} sm={12} className='photo-card'>
+                            <img src={CardImage2} className='img-fluid card-image' alt='Card Image 2' />
+                        </Col>
+                        <Col lg={8} sm={12} className="d-flex align-items-center justify-content-center">
+                            <div className='card-content text-end'>
+                                <h2 className='card-title p-3 text-end'>Birthday Blast from the Past</h2>
+                                <p className='card-text p-3 text-end'>Birthday Blast from the Past is a collaborative web application project crafted by a team of developers. It offers users a nostalgic journey through time by combining personal information with movie data. Built using HTML5, CSS3, JavaScript (with jQuery), Bootstrap, and leveraging APIs like TheMovieDB and YouTube Data, this project seamlessly integrates user input with dynamic content retrieval.</p>
+                                <Link to="https://segleston.github.io/birthday-blast-from-the-past/" className='button-text'> <button>Application</button> </Link>
+                                <Link to="https://github.com/segleston/birthday-blast-from-the-past/" className='button-text'> <button>Repository</button> </Link>
+                            </div>
+                        </Col>
+                    </Row>
+
+                    {/* </Link> */}
+                    {/* <Link to={`/project/3`}> */}
+                    <Row className="my-5 card-background">
+                        <Col lg={8} sm={12} className="d-flex align-items-center justify-content-center">
+                            <div className='card-content text-start'>
+                                <h2 className='card-title p-3 text-start'>Weather Dashboard</h2>
+                                <p className='card-text p-3 text-start'>The Weather Dashboard is a solo front-end project displaying current and 5-day forecast weather data for searched locations. Developed with JavaScript, HTML, and CSS, it integrates weather APIs to provide real-time information and featuring search history.</p>
+                                <Link to="https://segleston.github.io/weather-dashboard/" className='button-text'> <button>Application</button> </Link>
+                                <Link to="https://github.com/segleston/weather-dashboard/" className='button-text'> <button>Repository</button> </Link>
+                            </div>
+                        </Col>
+
+                        <Col lg={4} sm={12} className='photo-card'>
+                            <img src={CardImage3} className='img-fluid card-image' alt='Card Image 3' />
+                        </Col>
+                    </Row>
+                    {/* </Link> */}
                 </Container>
                 <div className="d-flex justify-content-center my-4">
-                <Link to="/portfolio"> <button>See More</button> </Link>
+                    <Link to="/portfolio" className='button-text'> <button>See More</button> </Link>
 
                 </div>
             </div>
